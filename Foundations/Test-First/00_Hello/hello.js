cdcefa9656e6af62f0d08bc292ecb4aa625dd18b
danmandel <danmandel@users.noreply.github.com>
2016-07-01 13:00:25 -0400

    Update hello.js

--- Foundations/Test-First/00_Hello/hello.js
+++ Foundations/Test-First/00_Hello/hello.js
@@ -1,7 +1,7 @@
-function hello(name){
-	if(typeof name === "undefined"){
+function hello(name) {
+	if (typeof name === "undefined") {
 		return "Hello!";
 	} else {
 		return `Hello, ${name}!`;
 	}
-}
+};
