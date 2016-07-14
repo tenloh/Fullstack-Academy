58cd9790fde7312158e6847830c59af868f962f9
danmandel <danmandel@users.noreply.github.com>
2016-07-13 22:02:40 -0400

    Update mammal.js

--- Foundations/Test-First/07_mammals/mammal.js
+++ Foundations/Test-First/07_mammals/mammal.js
@@ -1,12 +1,28 @@
 var Mammal = function(name){
     this.name = name;
     this.offspring = [];
-}
+};
 
 Mammal.prototype.sayHello = function(){
     return `My name is ${this.name}, I'm a Mammal`;
-}
+};
 
 Mammal.prototype.haveBaby = function(){
-    var baby = Object.create(this)
-}
+    var baby = new Mammal(`Baby ${this.name}`);
+    this.offspring.push(baby);
+    return baby;
+};
+
+var Cat = function(name, color){
+    Mammal.call(this,name)
+    this.color = color;
+};
+
+Cat.prototype = Object.create(Mammal.prototype);
+Cat.prototype.constructor = Cat;
+Cat.prototype.haveBaby = function(color){
+    var baby = new Cat(`Baby ${this.name}`,color);
+    this.offspring.push(baby);
+    return baby;
+};
+
