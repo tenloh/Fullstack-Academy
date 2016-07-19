617c5a65229de0af614d8aa6a0745e2e7902dade
danmandel <danmandel@users.noreply.github.com>
2016-07-19 19:12:03 -0400

    Update event_emitter.js

--- TestFirstAssessmentPractice/03_event_emitter/event_emitter.js
+++ TestFirstAssessmentPractice/03_event_emitter/event_emitter.js
@@ -1,1 +1,18 @@
+var MyEventEmitter = function(){
+    this.events = {};
+};
+
+MyEventEmitter.prototype.addListener = function(event, callback){
+    if (!this.events[event]){
+        this.events[event] = [];
+    }
+    this.events[event].push(callback);
+};
+
+MyEventEmitter.prototype.emit = function(event){
+    var args = Array.prototype.slice.call(arguments);
+    for (var i=0;i<this.events[event].length;i++){
+        this.events[event][i].apply(null, args.slice(1,args.length));         
+    };
+};
 
