3f966e41dcc92bba6d437047ca80b0609851963c
danmandel <danmandel@users.noreply.github.com>
2016-07-18 22:58:09 -0400

    Update sometimes.js

--- TestFirstAssessmentPractice/01_sometimes/sometimes.js
+++ TestFirstAssessmentPractice/01_sometimes/sometimes.js
@@ -1,1 +1,14 @@
-
+var sometimes = function(func){
+    var timesRan = 0;
+    var newFunc = function(){
+        timesRan++;
+        if (timesRan < 4 || timesRan %2 !== 0){
+            return func.apply(null,arguments);
+        } 
+        else {
+            return 'I do not know!'
+        }
+    };
+    
+    return newFunc;
+};
