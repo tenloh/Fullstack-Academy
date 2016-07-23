30d5dc9caabf37696f6e38e87bcef1ecc70f5454
danmandel <danmandel@users.noreply.github.com>
2016-07-22 20:18:07 -0400

    Update pig_latin.js

--- TestFirstAssessmentPractice/00_pig_latin/pig_latin.js
+++ TestFirstAssessmentPractice/00_pig_latin/pig_latin.js
@@ -1,1 +1,20 @@
+var translate = function(str){
+    var result = '';
+    var wordArr = str.split(' ');
+    var resultArr = wordArr.map(word => translateToPigLatin(word));
+    result = resultArr.join(' ');   
+    return result;
+};
 
+var translateToPigLatin = function(word){
+    var consonant = /[^aeiou]/;
+    var newWord = word;
+    var currentLetterIndex = 0;
+    while (consonant.test(word[currentLetterIndex]) || word[currentLetterIndex-1] === 'q'){
+        currentLetterIndex++;
+    };
+    newWord = word.substring(currentLetterIndex, word.length) + word.substr(0,currentLetterIndex);
+    newWord += 'ay';
+    return newWord;
+};
+  
